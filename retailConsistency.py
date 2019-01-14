faf3e7a186758a2ebd81fe3d70430316becb3fe3
SunGitHub7 <45933453+SunGitHub7@users.noreply.github.com>
2019-01-14 11:01:07 +0530

    Add files via upload

---
+++ retailConsistency.py
@@ -0,0 +1,33 @@
+import pandas as pd
+df = pd.read_csv('E:\ThinkBumble\Online Retail.csv')
+df['month'] = df['InvoiceDate'].str.extract("/(\d+)/")  # month column having value extracted from data string.
+# print(df)
+df2= df[['CustomerID', 'revenue','month']]  # select only desire features
+# print(df2)
+df3 = df2.groupby(['CustomerID', 'month']).mean()  # We need to know revenue for each customer for each month
+# print(df3)
+df4 = df3.unstack()  # cast row of month into columns
+df4.columns = df4.columns.droplevel(0)
+df4 = df4.reset_index().rename_axis(None, axis=1)
+df4['consistency'] = df4[['1','2','3']].std(axis=1)  # standard deviation
+df5 = df4[['CustomerID', 'consistency']]
+print(df4)
+#
+def consistent(df):
+    if df > 10:
+        return 'No'
+    else:
+        return 'Yes'
+
+
+df4['consistant'] = df4['consistency'].apply(lambda x: consistent(x))
+
+print(df4)
+'''CustomerID          1          2          3  consistency consistant
+0       13694   4.371667  11.991667  14.020000     5.087057         No
+1       13758   3.218824  19.061765  15.279412     8.274084         No
+2       14307  17.940000  45.720000  20.136000    15.443941        Yes
+3       17908   4.297222  27.840000   7.783333    12.706201        Yes
+4       17920   4.891500  15.013000  58.454500    28.456410        Yes
+5       17968   5.326207  15.420690  15.398276     5.821593         No '''
+
